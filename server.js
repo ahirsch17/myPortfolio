92f7ed15537ec05c8e29298d7b8df30be5363636
Alexis <alexishirsch@vt.edu>
2025-10-29 16:24:50 -0400

    Complete portfolio redesign: add profile section, interactive projects page with modals, and embedded Flappy Bird-style game with upgrade shop

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
-
 app.get('/projects', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'projects.html'));
 });
