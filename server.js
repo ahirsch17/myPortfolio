23e1f9a6fc49e37f6ae00ec45dbf42c44284aae7
Alexis <alexishirsch@vt.edu>
2025-11-12 13:38:18 -0500

    latest

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/playground', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'playground', 'effort-score-demo.html'));
+});
+
 app.get('/projects', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'projects.html'));
 });
